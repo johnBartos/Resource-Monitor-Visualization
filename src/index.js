042f50bd3484627fa42ce1db01e00d4245b4755d
John Bartos <jbartos7@gmail.com>
2016-03-29 19:05:48 -0400

    Basic env setup + hello world

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './containers/App';
+
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('root')
+);
