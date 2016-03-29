a5bbce87fd28e73e6348e8f22f8a11c2623cd6a7
John Bartos <jbartos7@gmail.com>
2016-03-29 19:13:38 -0400

    Add eslint

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 
 class App extends Component {
-    render() {
-        return (
-            <h1>Hello</h1>
-        )
-    }
+  render() {
+    return (
+      <section id="App">
+        <h1>Hello</h1>
+        {this.props.children}
+      </section>
+    );
+  }
 }
 
 export default App;
