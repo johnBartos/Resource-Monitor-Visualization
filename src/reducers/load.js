e008ea95ca6dd94988333a2c2a40e1ebcf571168
John Bartos <jbartos7@gmail.com>
2016-03-30 19:35:00 -0400

    Implement historical load reducer

--- src/reducers/load.js
+++ src/reducers/load.js
@@ -1,10 +1,21 @@
 const initialState = {
-  duration: 10 * 60,
+  threshold: 10 * 60 * 1000, // milliseconds
   readings: []
 };
 
+const pruneReadings = (readings, cutoff) => {
+  const cutoffDate = Date.now() - cutoff;
+  return readings.filter(e => e.date >= cutoffDate);
+};
+
+
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'READING_RECEIVED':
+      return {
+        ...state,
+        readings: pruneReadings([...state.readings, action.reading], state.threshold)
+      };
 
     default:
       return state;
--- src/reducers/tests/load-spec.js
+++ src/reducers/tests/load-spec.js
@@ -3,16 +3,65 @@ import load from '../load';
 
 describe('load reducer', function () {
   let initialState;
+  let now;
 
   beforeEach(function () {
     initialState = {
-      duration: 10 * 60,
+      threshold: 10 * 60 * 1000,
       readings: []
     };
+    now = Date.now();
   });
 
   it('returns initial state', function () {
     const actual = load(initialState, {});
     expect(actual).to.deep.equal(initialState);
+  });
+
+  it('handles READING_RECEIVED', function () {
+    const reading = {
+      date: now,
+      one: 1,
+      five: 5,
+      ten: 10
+    };
+
+    const expected = {
+      ...initialState,
+      readings: [reading]
+    };
+
+    const actual = load(initialState, {
+      type: 'READING_RECEIVED',
+      reading
+    });
+
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('prunes readings below the threshold on READING_RECEIVED', function () {
+    const oldReading = {
+      date: now - initialState.threshold
+    };
+    const newReading = {
+      date: now
+    };
+
+    initialState = {
+      ...initialState,
+      readings: [oldReading]
+    };
+
+    const expected = {
+      ...initialState,
+      readings: [newReading]
+    };
+
+    const actual = load(initialState, {
+      type: 'READING_RECEIVED',
+      reading: newReading
+    });
+
+    expect(actual).to.deep.equal(expected);
   });
 });
